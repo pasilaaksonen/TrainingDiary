e0a0e1dc4f69f73e3d03a83a41d719377528886a
pasilaaksonen <pasi.laaksonen@edu.turkuamk.fi>
2021-11-13 03:58:14 +0200

    Create trainer-model.js

---
+++ back-end/models/trainer-model.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const trainerSchema = mongoose.Schema({
+   pmv: String,
+   suoritukset_yht: Number,
+   suorittaja_lkm: Number,
+   isProfessional: Boolean,
+});
+
+const TrainerData = mongoose.model('trainingdata', trainerSchema);
+//
+export default TrainerData;

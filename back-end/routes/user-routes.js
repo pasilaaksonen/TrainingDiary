f1bdfddad7538fc04c8a1ac84d4a50ae3c134d7c
apbd <60779371+apbd@users.noreply.github.com>
2021-11-21 14:27:21 +0200

    Added profile page

--- back-end/routes/user-routes.js
+++ back-end/routes/user-routes.js
@@ -1,16 +1,27 @@
 import express from 'express';
-import { register, getData, readData, readData2, readData3, insertNewTrainingData, deleteTrainingData, editTraining } from '../controllers/user-controller.js';
+import {
+  register,
+  getData,
+  readData,
+  readData2,
+  readData3,
+  readProfile,
+  insertNewTrainingData,
+  deleteTrainingData,
+  editTraining,
+} from '../controllers/user-controller.js';
 
 const router = express.Router();
 
 // router.post('/login', usersControllers.login);
 router.post('/register', register);
 router.post('/read', getData);
-router.get('/result/amateurs', readData)
-router.get('/result/professionals', readData2)
-router.get('/result/omat', readData3)
-router.post('/addnew', insertNewTrainingData)
-router.delete('/delete/:id', deleteTrainingData)
+router.get('/result/amateurs', readData);
+router.get('/result/professionals', readData2);
+router.get('/result/omat', readData3);
+router.get('/result/profile', readProfile);
+router.post('/addnew', insertNewTrainingData);
+router.delete('/delete/:id', deleteTrainingData);
 router.put('/update', editTraining);
 
 export default router;
---
+++ front-end/src/components/OmaProfiili.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import Axios from 'axios';
+
+const OmaProfiili = () => {
+  //
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    Axios.get('http://localhost:5000/user/result/profile').then(response => {
+      console.log('selected user', response.data[0]);
+
+      // TODO: kirjautuneen käyttäjän valinta
+      setUser({
+        name: response.data[0].name,
+        lastname: response.data[0].lastname,
+        isProfessional: response.data[0].isProfessional,
+      });
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Oma profiili</h1>
+      <Card style={{ width: '18rem' }}>
+        {/* <Card.Img variant='top' src='holder.js/100px180?text=Image cap' /> */}
+        <Card.Body>
+          <Card.Title>
+            {user.name} {user.lastname}
+          </Card.Title>
+          {/* <Card.Text>Profile description</Card.Text> */}
+        </Card.Body>
+        <ListGroup className='list-group-flush'>
+          <ListGroupItem>
+            Nimike: {user.isProfessional ? 'Ammattilainen' : 'Harrastelija'}
+          </ListGroupItem>
+        </ListGroup>
+        {/* <Card.Body>
+          <Card.Link href='#'>Card Link</Card.Link>
+          <Card.Link href='#'>Another Link</Card.Link>
+        </Card.Body> */}
+      </Card>
+    </div>
+  );
+};
+
+export default OmaProfiili;

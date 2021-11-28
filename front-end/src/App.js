b61b927d9501835fb050efa46edead00d244058b
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-11-28 19:40:09 +0200

    Merge branch 'main' into Yolanda

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,40 +3,57 @@ import Etusivu from './components/Etusivu';
 import AmmattilaistenTulokset from './components/AmmattilaistenTulokset';
 import HarrastajienTulokset from './components/HarrastajienTulokset';
 import OmatTreenit from './components/OmatTreenit';
+import OmaProfiili from './components/OmaProfiili';
 import Navigointi from './components/Navigointi';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProtectedRoute from './ProtectedPages/ProtectedRoute';
 
 const App = () => {
-
-    const [isLogged, setIsLogged] = useState(false)
-    const [isLoggedAmmattilainen, setIsLoggedAmmattilainen] = useState(false)
-    const [name, setName] = useState("");
-    //
-    return (
-        <>
-        <Router>
-            <Navigointi 
-            setIsLogged={setIsLogged} 
-            isLogged={isLogged}
-            isLoggedAmmattilainen={isLoggedAmmattilainen}
-            setIsLoggedAmmattilainen={setIsLoggedAmmattilainen}
+  const [isLogged, setIsLogged] = useState(false);
+  const [isLoggedAmmattilainen, setIsLoggedAmmattilainen] = useState(false);
+  const [name, setName] = useState('');
+  //
+  return (
+    <>
+      <Router>
+        <Navigointi
+          setIsLogged={setIsLogged}
+          isLogged={isLogged}
+          isLoggedAmmattilainen={isLoggedAmmattilainen}
+          setIsLoggedAmmattilainen={setIsLoggedAmmattilainen}
+          name={name}
+          setName={setName}
+        />
+        <Switch>
+          <Route path='/' exact render={() => <Etusivu />} />
+          <ProtectedRoute
+            path='/ammattilaisten_tulokset'
+            component={AmmattilaistenTulokset}
+            isAuth={isLoggedAmmattilainen}
+          />
+          <Route
+            path='/harrastajien_tulokset'
+            exact
+            render={() => <HarrastajienTulokset />}
+          />
+          <ProtectedRoute
+            path='/omat_treenit'
+            component={OmatTreenit}
+            isAuth={isLogged}
             name={name}
-            setName={setName}
-            />
-            <Switch>
-                <Route path='/' exact render={() => <Etusivu />}/>
-                <ProtectedRoute path='/ammattilaisten_tulokset' component={AmmattilaistenTulokset} isAuth={isLoggedAmmattilainen}/>
-                <Route path='/harrastajien_tulokset' exact render={() => <HarrastajienTulokset />}/>
-                <ProtectedRoute path='/omat_treenit' 
-                component={OmatTreenit} 
-                isAuth={isLogged}
-                name={name}
-                isLoggedAmmattilainen = {isLoggedAmmattilainen}
-                />
-            </Switch>
-        </Router>
-        </>
-)};
+            isLoggedAmmattilainen={isLoggedAmmattilainen}
+          />
+          <ProtectedRoute
+            path='/oma_profiili'
+            component={OmaProfiili}
+            isAuth={isLogged}
+            name={name}
+            isLoggedAmmattilainen={isLoggedAmmattilainen}
+          />
+        </Switch>
+      </Router>
+    </>
+  );
+};
 
 export default App;

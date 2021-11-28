d73956004b6243bf3639abf217ec6e8a1523d3ed
pasilaaksonen <pasi.laaksonen@edu.turkuamk.fi>
2021-11-28 21:21:04 +0200

    Login and removed old useless code

--- front-end/src/components/Navigointi.js
+++ front-end/src/components/Navigointi.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
-import Axios from 'axios';
 import { FaHome } from "react-icons/fa";
 import { BiLogIn, BiLogOut } from "react-icons/bi";
+import trainingDiaryServices from '../services/trainingDiary';
+import loginServices from '../services/login';
+import signinServices from '../services/signin';
 
 const Navigointi = (props) => {
 //
@@ -18,64 +20,92 @@ const Navigointi = (props) => {
     const [loggedIn, setLoggedIn] = useState(false);
     const [loggedInAs, setLoggedInAs] = useState("");
 
-    const handleCloseSignIn = () => {
 
-        setsignInDialog(false);
-    }
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+        try {
+          const user = await loginServices.login({
+            email, password,
+          })
+          window.localStorage.setItem(
+            'loggedBlogAppUser', JSON.stringify(user)
+          )
+          trainingDiaryServices.setToken(user.token)
+          console.log(user)
+          props.setUser(user)
+        //   setUsername('')
+        //   setPassword('')
+
+        setLoggedIn(true);
+        setLoggedInAs(user.name);
+        props.setIsLoggedAmmattilainen(user.isProfessional);
+        props.setName(user.name);
+        props.setIsLogged(true);
+
+        console.log('succesfully logged in')
+        handleCloseSignIn();
+        } catch (exception) {
+            console.log('wrong username or password')
+            handleCloseSignIn();
+        }
+      }
+
+    const handleCloseSignIn = () => setsignInDialog(false);
     const handleCloseSignUp = () => setsignUpDialog(false);
-
+    const handleShowSignIn = () => setsignInDialog(true);
+    
     const handleRegisterDialog = () => {
         setsignInDialog(false);
         setsignUpDialog(true);
     }
 
-    const handleShowSignIn = () => setsignInDialog(true);
-
+    
     const registerUser = () => {
 
         if (!props.name || !lastname || !email || !password) 
           return;
         
-        Axios.post("http://localhost:5000/user/register", {
-          name: props.name,
-          lastname: lastname,
-          email: email,
-          password: password,
-          isProfessional: registerCheckBox,
-        });
-      };
-
-    const readUser = () => {
-
-        Axios.post("http://localhost:5000/user/read", {
-            email: email, 
+        signinServices.signin({
+            name: props.name,
+            lastname: lastname,
+            email: email,
             password: password,
+            isProfessional: registerCheckBox,  
         })
-            .then((response) => {  
-            if (response.data === "Kirjautuminen epäonnistui") {
-                console.log("Tarkista kirjautumistiedot")
-                return;
-            } else {
-            setLoggedIn(true);
-            setLoggedInAs(response.data[0]);
-            props.setIsLoggedAmmattilainen(response.data[1]);
-            props.setName(response.data[0]);
-            props.setIsLogged(true);
-            }
-            });    
-    }
-    
-    const handleSignUp = () => {
+    };
 
-        if (!email || !password) {
-            console.log("Tyhjät kentät ei käy");
-            handleCloseSignIn();
-            return;
-        } else {
-            readUser();
-            handleCloseSignIn();
-        }
-    }
+    // const readUser = () => {
+
+    //     Axios.post("http://localhost:5000/user/read", {
+    //         email: email, 
+    //         password: password,
+    //     })
+    //         .then((response) => {  
+    //         if (response.data === "Kirjautuminen epäonnistui") {
+    //             console.log("Tarkista kirjautumistiedot")
+    //             return;
+    //         } else {
+    //         setLoggedIn(true);
+    //         setLoggedInAs(response.data[0]);
+    //         props.setIsLoggedAmmattilainen(response.data[1]);
+    //         props.setName(response.data[0]);
+    //         props.setIsLogged(true);
+    //         }
+    //         });    
+    // }
+    
+    // const handleSignUp = () => {
+
+    //     if (!email || !password) {
+    //         console.log("Tyhjät kentät ei käy");
+    //         handleCloseSignIn();
+    //         return;
+    //     } else {
+    //         readUser();
+    //         handleCloseSignIn();
+    //     }
+    // }
 
     const handleRegister = () => {
         registerUser();
@@ -139,7 +169,7 @@ const Navigointi = (props) => {
                     <input type="password" onChange={(e) => setPasword(e.target.value)}></input>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleSignUp}>
+                    <Button variant="secondary" onClick={handleLogin}>
                         Kirjaudu
                     </Button>
                     <Button variant="secondary" onClick={handleRegisterDialog}>

d5c454e1d5c7574d659be7ebb3c123dbe96253a6
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2021-12-12 23:41:37 +0200

    Fixed footer css

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -6,14 +6,12 @@ import git from '../images/25231.svg';
 const Footer = () => {
 
     return (
-        <div className="footer">
-            <h4 className='footerText'>
+        <div className='footer'>
+            <h6 className='footerText'>
                 Back-End Development project by Pasi Laaksonen, Albert Puustinen, Samuel Jumppanen, Yolanda Theodorakis
-            </h4>
-            <a href="https://github.com/pasilaaksonen/TrainingDiary">
-                <img src={git} alt="" width="3%" 
-                style={{backgroundColor: "grey", marginTop: "15px"}}>
-                </img>
+            </h6>
+            <a href='https://github.com/pasilaaksonen/TrainingDiary'>
+                <img src={git} alt='github' width='25em' style={{backgroundColor: 'grey'}} />
             </a>
         </div>
     )

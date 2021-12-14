609d2d9bcb3a64ad6024dc2fd34ba70e661db3b0
pasilaaksonen <pasi.laaksonen@edu.turkuamk.fi>
2021-12-14 20:17:03 +0200

    Added user field containing writed id

--- back-end/models/own-training.js
+++ back-end/models/own-training.js
@@ -1,12 +1,34 @@
 import mongoose from 'mongoose';
 
 const ownTrainingSchema = mongoose.Schema({
-   name: String,
-   pvm: String,
-   laji: String,
-   suoritukset_yht: Number,
-   paino: Number,
-   isProfessional: Boolean,
+   name: {
+      type: String,
+      required: true
+   },
+   pvm: {
+      type: String,
+      required: true
+   },
+   laji: {
+      type: String,
+      required: true
+   },
+   suoritukset_yht: {
+      type: Number,
+      required: true
+   },
+   paino: {
+      type: Number,
+      required: true
+   },
+   isProfessional: {
+      type: Boolean,
+      required: true
+   },
+   user: {    
+      type: mongoose.Schema.Types.ObjectId,    
+      ref: 'userdata'  
+    }
 });
 
 const OwnTrainingData = mongoose.model('owntrainingdata', ownTrainingSchema);
